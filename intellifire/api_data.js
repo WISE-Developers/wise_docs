5e374a4653fa5c4b7907e471cd575fd0d408fc83
Franco Nogarin <franco_nogarin@gov.nt.ca>
2019-09-18 16:57:48 -0600

    adding intellifire api docs

---
+++ intellifire/api_data.js
@@ -0,0 +1,73 @@
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./doc/main.js",
+    "group": "E__localcode_gitrepos_intellifire_lrgs_doc_main_js",
+    "groupTitle": "E__localcode_gitrepos_intellifire_lrgs_doc_main_js",
+    "name": ""
+  },
+  {
+    "type": "post",
+    "url": "/schedule/schedule/",
+    "title": "",
+    "version": "1.0.0",
+    "name": "schedule",
+    "group": "weather",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "params",
+            "description": "<p>NESDIS ID to schedule or all for all stations.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "schedule",
+            "description": "<p>the schedule set for the station.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "schedules",
+            "description": "<p>the schedules set for each of all the stations</p>"
+          }
+        ]
+      }
+    },
+    "filename": "./models/schedule_m.js",
+    "groupTitle": "weather"
+  }
+] });
