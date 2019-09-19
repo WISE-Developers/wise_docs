b56a51dad2fe66402781330529321381e20a7f6c
Franco Nogarin <franco_nogarin@gov.nt.ca>
2019-09-19 08:57:29 -0600

    Intellifire API update

--- intellifire/api_project.js
+++ intellifire/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "Handles primary weather functions",
   "title": "Intellifire Schedule",
-  "url": "https://spydmobile.bitbucket.io/intellifire/",
+  "url": "",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-09-18T23:03:43.306Z",
+    "time": "2019-09-19T14:56:37.033Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
